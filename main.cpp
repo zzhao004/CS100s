b76df7aed93d9c29ca64f762247f96c54eedf4a3
Nathan Zhao <zzhao004@hammer.cs.ucr.edu>
2017-10-04 19:22:27 -0700

    solved merge conflict between useriput and master branches

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
 	std::string name
 	std::cout << "What is your name?" << std::endl;
 	std::cin >> name;
-	std::cout << "hello git" << std::endl;
+	std::cout << "hello" << name << "!" << std::endl;
 	return 0;
 }
 
